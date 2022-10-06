12555f61fad7da74e0bee8e002df388e22ceb91d
Maxine Franklin <GitHub@oscarfranklin.co.uk>
2022-10-06 09:34:44 +0100

    V0.1.0
    Creation of physical world
    Started creation of day/night cycle
    Created required materials
    Setup vs files
    Creation of Git Project

---
+++ Assets/Scripts/skyLight.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skyLight : MonoBehaviour
+{
+    public Transform Target;
+    public Quaternion Force;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /*Vector3 relativePos = (Target.position + Force) - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 3 * Time.deltaTime);*/
+        
+        // -->this.transform.rotation. = Force;
+
+        //transform.LookAt(Target);
+        //GetComponent<Rigidbody>().AddRelativeForce(Force);
+    }
+}
